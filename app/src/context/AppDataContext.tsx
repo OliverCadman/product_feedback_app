209ea491b6765d87ddc0022b2fcdfce21cd18eb4
Oliver Cadman <o.cadman@live.co.uk>
2023-08-11 17:02:44 +0100

    Added static UI - mobile nav header

---
+++ app/src/context/AppDataContext.tsx
@@ -0,0 +1,5 @@
+import React, { useContext, createContext } from "react";
+
+interface AppDataContextType {
+    
+}
---
+++ app/src/pages/ProductFeedback/ProductFeedback.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import NavMob from '../../components/mobile/Nav/NavMob';
+
+const ProductFeedback: React.FC = () => {
+  return (
+    <NavMob />
+  )
+}
+
+export default ProductFeedback
