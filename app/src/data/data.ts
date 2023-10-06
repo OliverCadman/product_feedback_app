6878ce0d252cdfa00ed586a8a253ed7749fcec53
Oliver Cadman <o.cadman@live.co.uk>
2023-10-06 08:32:49 +0100

    Corrected path to newly-located image files

--- app/src/data/data.ts
+++ app/src/data/data.ts
@@ -4,7 +4,7 @@ import { IAppData } from "../types/AppData/appdata.types";
 const data: IAppData = {
   currentUser: {
     userDetails: {
-      image: "/assets/user-images/image-zena.jpg",
+      image: "/product_feedback_app/assets/user-images/image-zena.jpg",
       name: "Zena Kelley",
       username: "velvetround"
     },
@@ -24,7 +24,7 @@ const data: IAppData = {
           content:
             "Awesome idea! Trying to find framework-specific projects within the hubs can be tedious",
           user: {
-            image: "/assets/user-images/image-suzanne.jpg",
+            image: "/product_feedback_app/assets/user-images/image-suzanne.jpg",
             name: "Suzanne Chang",
             username: "upbeat1811"
           },
@@ -35,7 +35,7 @@ const data: IAppData = {
           content:
             "Please use fun, color-coded labels to easily identify them at a glance",
           user: {
-            image: "/assets/user-images/image-thomas.jpg",
+            image: "/product_feedback_app/assets/user-images/image-thomas.jpg",
             name: "Thomas Hood",
             username: "brawnybrave"
           },
@@ -57,7 +57,7 @@ const data: IAppData = {
           content:
             "Also, please allow styles to be applied based on system preferences. I would love to be able to browse Frontend Mentor in the evening after my device’s dark mode turns on without the bright background it currently has.",
           user: {
-            image: "/assets/user-images/image-elijah.jpg",
+            image: "/product_feedback_app/assets/user-images/image-elijah.jpg",
             name: "Elijah Moss",
             username: "hexagon.bestagon"
           },
@@ -68,7 +68,8 @@ const data: IAppData = {
                 "While waiting for dark mode, there are browser extensions that will also do the job. Search for 'dark theme' followed by your browser. There might be a need to turn off the extension for sites with naturally black backgrounds though.",
               replyingTo: "hummingbird1",
               user: {
-                image: "/assets/user-images/image-anne.jpg",
+                image:
+                  "/product_feedback_app/assets/user-images/image-anne.jpg",
                 name: "Anne Valentine",
                 username: "annev1990"
               }
@@ -80,7 +81,7 @@ const data: IAppData = {
           content:
             "Second this! I do a lot of late night coding and reading. Adding a dark theme can be great for preventing eye strain and the headaches that result. It’s also quite a trend with modern apps and  apparently saves battery life.",
           user: {
-            image: "/assets/user-images/image-james.jpg",
+            image: "/product_feedback_app/assets/user-images/image-james.jpg",
             name: "James Skinner",
             username: "hummingbird1"
           },
@@ -91,7 +92,8 @@ const data: IAppData = {
                 "While waiting for dark mode, there are browser extensions that will also do the job. Search for 'dark theme' followed by your browser. There might be a need to turn off the extension for sites with naturally black backgrounds though.",
               replyingTo: "hummingbird1",
               user: {
-                image: "/assets/user-images/image-anne.jpg",
+                image:
+                  "/product_feedback_app/assets/user-images/image-anne.jpg",
                 name: "Anne Valentine",
                 username: "annev1990"
               }
@@ -102,7 +104,8 @@ const data: IAppData = {
                 "Good point! Using any kind of style extension is great and can be highly customizable, like the ability to change contrast and brightness.",
               replyingTo: "annev1990",
               user: {
-                image: "/assets/user-images/image-ryan.jpg",
+                image:
+                  "/product_feedback_app/assets/user-images/image-ryan.jpg",
                 name: "Ryan Welles",
                 username: "voyager.344"
               }
@@ -124,7 +127,7 @@ const data: IAppData = {
           content:
             "Much easier to get answers from devs who can relate, since they've either finished the challenge themselves or are in the middle of it.",
           user: {
-            image: "/assets/user-images/image-george.jpg",
+            image: "/product_feedback_app/assets/user-images/image-george.jpg",
             name: "George Partridge",
             username: "soccerviewer8"
           },
@@ -145,7 +148,7 @@ const data: IAppData = {
           content:
             "Right now, there is no ability to add images while giving feedback which isn't ideal because I have to use another app to show what I mean",
           user: {
-            image: "/assets/user-images/image-javier.jpg",
+            image: "/product_feedback_app/assets/user-images/image-javier.jpg",
             name: "Javier Pollard",
             username: "warlikeduke"
           },
@@ -156,7 +159,7 @@ const data: IAppData = {
           content:
             "Yes I'd like to see this as well. Sometimes I want to add a short video or gif to explain the site's behavior..",
           user: {
-            image: "/assets/user-images/image-roxanne.jpg",
+            image: "/product_feedback_app/assets/user-images/image-roxanne.jpg",
             name: "Roxanne Travis",
             username: "peppersprime32"
           },
@@ -177,7 +180,8 @@ const data: IAppData = {
           content:
             "I also want to be notified when devs I follow submit projects on FEM. Is in-app notification also in the pipeline?",
           user: {
-            image: "/assets/user-images/image-victoria.jpg",
+            image:
+              "/product_feedback_app/assets/user-images/image-victoria.jpg",
             name: "Victoria Mejia",
             username: "arlen_the_marlin"
           },
@@ -188,7 +192,8 @@ const data: IAppData = {
                 "Bumping this. It would be good to have a tab with a feed of people I follow so it's easy to see what challenges they’ve done lately. I learn a lot by reading good developers' code.",
               replyingTo: "arlen_the_marlin",
               user: {
-                image: "/assets/user-images/image-zena.jpg",
+                image:
+                  "/product_feedback_app/assets/user-images/image-zena.jpg",
                 name: "Zena Kelley",
                 username: "velvetround"
               }
@@ -200,7 +205,7 @@ const data: IAppData = {
           content:
             "I've been saving the profile URLs of a few people and I check what they’ve been doing from time to time. Being able to follow them solves that",
           user: {
-            image: "/assets/user-images/image-jackson.jpg",
+            image: "/product_feedback_app/assets/user-images/image-jackson.jpg",
             name: "Jackson Barker",
             username: "countryspirit"
           },
@@ -231,7 +236,8 @@ const data: IAppData = {
           content:
             "This would be awesome! It would be so helpful to see an overview of my code in a way that makes it easy to spot where things could be improved.",
           user: {
-            image: "/assets/user-images/image-victoria.jpg",
+            image:
+              "/product_feedback_app/assets/user-images/image-victoria.jpg",
             name: "Victoria Mejia",
             username: "arlen_the_marlin"
           },
@@ -242,7 +248,7 @@ const data: IAppData = {
           content:
             "Yeah, this would be really good. I'd love to see deeper insights into my code!",
           user: {
-            image: "/assets/user-images/image-jackson.jpg",
+            image: "/product_feedback_app/assets/user-images/image-jackson.jpg",
             name: "Jackson Barker",
             username: "countryspirit"
           },
@@ -264,7 +270,7 @@ const data: IAppData = {
           content:
             "Having a path through the challenges that I could follow would be brilliant! Sometimes I'm not sure which challenge would be the best next step to take. So this would help me navigate through them!",
           user: {
-            image: "/assets/user-images/image-george.jpg",
+            image: "/product_feedback_app/assets/user-images/image-george.jpg",
             name: "George Partridge",
             username: "soccerviewer8"
           },
@@ -286,7 +292,7 @@ const data: IAppData = {
           content:
             "I haven't built a portfolio site yet, so this would be really helpful. Might it also be possible to choose layout and colour themes?!",
           user: {
-            image: "/assets/user-images/image-ryan.jpg",
+            image: "/product_feedback_app/assets/user-images/image-ryan.jpg",
             name: "Ryan Welles",
             username: "voyager.344"
           },
@@ -307,7 +313,7 @@ const data: IAppData = {
           content:
             "This would be great! At the moment, I'm just starting challenges in order to save them. But this means the My Challenges section is overflowing with projects and is hard to manage. Being able to bookmark challenges would be really helpful.",
           user: {
-            image: "/assets/user-images/image-suzanne.jpg",
+            image: "/product_feedback_app/assets/user-images/image-suzanne.jpg",
             name: "Suzanne Chang",
             username: "upbeat1811"
           },
@@ -337,7 +343,8 @@ const data: IAppData = {
           content:
             "I'd love to see this! It always makes me so happy to see little details like these on websites.",
           user: {
-            image: "/assets/user-images/image-victoria.jpg",
+            image:
+              "/product_feedback_app/assets/user-images/image-victoria.jpg",
             name: "Victoria Mejia",
             username: "arlen_the_marlin"
           },
@@ -348,7 +355,8 @@ const data: IAppData = {
                 "Me too! I'd also love to see celebrations at specific points as well. It would help people take a moment to celebrate their achievements!",
               replyingTo: "arlen_the_marlin",
               user: {
-                image: "/assets/user-images/image-suzanne.jpg",
+                image:
+                  "/product_feedback_app/assets/user-images/image-suzanne.jpg",
                 name: "Suzanne Chang",
                 username: "upbeat1811"
               }
